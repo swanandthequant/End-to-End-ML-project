b5f471cda55ee27c1858d3c1f08f8d0ca6f05397
Swanand Pankaj patil <swanandpatil409@gmail.com>
2025-10-27 13:35:36 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,3 @@
-import logging
 import sys
 
 def error_message_detail(error,error_detail:sys):
@@ -16,8 +15,4 @@ class CustomException(Exception):
         def __str__(self):
             return self.error_message
         
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        raise CustomException(e, sys)
+
